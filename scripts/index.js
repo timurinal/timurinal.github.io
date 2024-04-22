a744ab4cfcefbde51e604609125c628936736627
Timur Inal <timurinal09@gmail.com>
2024-04-22 14:15:17 +0100

    Updated website layout for mobile and desktop views, added navbar and home intro blocks, revised navigation menu links, and introduced random background selection.

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     var userAgent = navigator.userAgent;
 
+    // TODO: REMOVE THIS, IT IS ONLY FOR TESTING MOBILE LAYOUT
+    //document.body.classList.add('mobile')
+    document.body.classList.add('desktop')
+    //return;
+
     if (/android/i.test(userAgent)) {
         document.body.classList.add('mobile');
     } else if (/iphone|ipad|ipod/i.test(userAgent)) {
@@ -11,5 +16,22 @@ document.addEventListener("DOMContentLoaded", function() {
         document.body.classList.add('desktop');
     } else {
         document.body.classList.add('desktop');
+    }
+
+    // Select random background image only if the current site is on a desktop
+    if (document.body.classList.contains('desktop')) {
+        const backgrounds = ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png'];
+
+        // Path to the images directory
+        const path = 'img/home/';
+
+        // Select a random background image
+        const randomImage = backgrounds[Math.floor(Math.random() * backgrounds.length)];
+
+        // Get the .home-intro element
+        const homeIntro = document.querySelector('.home-intro');
+
+        // Set the random background image
+        homeIntro.style.backgroundImage = `url('${path}${randomImage}')`;
     }
 });
